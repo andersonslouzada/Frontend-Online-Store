070ab2a6ebe1b2dbf8ffe37055f65967ab5eabf3
Wellison M. Dias <wellisonmdias@gmail.com>
2023-03-17 16:31:45 -0300

    cumpre requisito 3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Home extends Component {
   render() {
@@ -13,6 +14,14 @@ export default class Home extends Component {
           type="text"
           placeholder="Digite algum termo de pesquisa ou esolha uma categoria"
         />
+        <Link to="/cart" data-testid="shopping-cart-button">
+          <button
+            type="button"
+          >
+            carrinho
+          </button>
+        </Link>
+
       </div>
     );
   }
